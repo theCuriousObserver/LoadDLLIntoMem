48c6a89ab866a786f4c5f47d3d68f4e4e9145e59
Pallav Avatarit <50916497+theCuriousObserver@users.noreply.github.com>
2022-08-15 17:13:45 +0100

    New commit

--- loadDLL.py
+++ loadDLL.py
@@ -163,13 +163,13 @@ def runDLL():
 
 # DEBUG:
 dll_names = runDLL()
+dll_names = list(set(dll_names))
 for i in range(len(dll_names)):
     dll_check = dll_names[i]
     try:
         WinDLL(dll_check)
-        print("Code Running")
     except Exception as error:
-        # print(dll_check)
+        print(dll_check)
         print(error)
     continue
 
--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-dll_names = runDLL()
-# for i in range(len(dll_names)):
-#     dll_check = dll_names[i]
-#     try:
-#         WinDLL(dll_check)
-#     except Exception as error:
-#         # print(dll_check)
-#         print(error)
-#         continue
