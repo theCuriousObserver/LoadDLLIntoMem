7e8bce3dc54857b9a0a9368efbde798765b79085
Pallav Avatarit <50916497+theCuriousObserver@users.noreply.github.com>
2022-08-15 17:04:41 +0100

    New change

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+dll_names = runDLL()
+# for i in range(len(dll_names)):
+#     dll_check = dll_names[i]
+#     try:
+#         WinDLL(dll_check)
+#     except Exception as error:
+#         # print(dll_check)
+#         print(error)
+#         continue
